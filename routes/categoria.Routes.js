72ebc27461ec9726608b71d0ccbe433f8a8b35ba
jmmb8 <jorgemanuel1986@gmail.com>
2025-08-24 22:07:29 -0500

    Corrige rutas en categoria.Routes.js

--- routes/categoria.Routes.js
+++ routes/categoria.Routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const CategoriaController = require('../controllers/categoriaController');
+const CategoriaController = require("../controllers/CategoriaController");
 
 // Rutas para categorías
-router.post('/categorias', CategoriaController.createCategoria);
-router.get('/categorias', CategoriaController.getCategorias);
-router.get('/categorias/:id', CategoriaController.getCategoriaById);
-router.put('/categorias/:id', CategoriaController.updateCategoria);
-router.delete('/categorias/:id', CategoriaController.deleteCategoria);
+router.post("/categorias", CategoriaController.createCategoria);
+router.get("/categorias", CategoriaController.getCategorias);
+router.get("/categorias/:id", CategoriaController.getCategoriaById);
+router.put("/categorias/:id", CategoriaController.updateCategoria);
+router.delete("/categorias/:id", CategoriaController.deleteCategoria);
 
 module.exports = router;
